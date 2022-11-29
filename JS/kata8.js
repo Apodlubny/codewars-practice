7b6a916aa8d2ac973e245407ba9847b09be7af5d
Apodlubny <apodlubny@gmail.com>
2022-11-29 23:07:18 +0200

    add kata

--- JS/kata8.js
+++ JS/kata8.js
@@ -1,4 +1,4 @@
-// The first one...couldn't be even more easy
+// 1. The first one...couldn't be even more easy
 
 const multiply = function (a, b) {
   return a * b;
@@ -10,7 +10,7 @@ console.log(multiply(2, 2)); //4
 console.log(multiply(3, 5)); //15
 // ===========================================================
 
-// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+// 2. Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 
 // Examples
 // Input: [1, 5.2, 4, 0, -1]
@@ -29,8 +29,9 @@ console.log(multiply(3, 5)); //15
 // What We're Testing
 // We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
 // Advanced users may find this extremely easy and can easily write this in one line.
+// ===========================================================
 
-// Sum Numbers
+// 3. Sum Numbers
 function sum(numbers) {
   let total = 0;
   for (const number of numbers) {
@@ -46,3 +47,26 @@ function sum(numbers) {
 console.log(sum([-1])); // -1
 console.log(sum([1, 5.2, 4, 0])); //10.2
 console.log(sum([1, 5.2, 4, 0, -1])); //9.2
+
+// ===================================================
+// 4. Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  return dna.replace(/T/g, "U");
+}
+
+// function DNAtoRNA(dna) {
+//   return dna.split("T").join("U");
+// }
+
+// ===================================================
