c5589b1d06f6b9f16bddb9c8676c17a03d6ca8dc
Apodlubny <apodlubny@gmail.com>
2022-12-13 19:27:18 +0200

    17. Sum of the first nth term of Series

--- kata7.js
+++ kata7.js
@@ -57,3 +57,44 @@ console.log(highAndLow("10 2 -1 -20")); //"10 -20"
 console.log(highAndLow("-1 -1")); //"-1 -1"
 console.log(highAndLow("-1 -1 0")); //"0 -1"
 console.log(highAndLow("42")); //"42"
+// ===================================================
+// 17. Sum of the first nth term of Series
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  let result = 0;
+  let reverage = 1;
+  for (let i = 0; i < n; i += 1) {
+    if (i === 0) {
+      result = 1;
+    } else {
+      reverage += 3;
+      result = result + 1 / reverage;
+    }
+  }
+  return result.toFixed(2);
+}
+
+// BP
+// function SeriesSum(n) {
+//   for (a = 0, i = 1; i <= n * 3; i += 3) a += 1 / i;
+//   return a.toFixed(2);
+// }
+
+console.log(SeriesSum(1)); //'1.00'
+console.log(SeriesSum(2)); //'1.25'
+console.log(SeriesSum(5)); //'1.57'
+console.log(SeriesSum(58)); //'2.4'
