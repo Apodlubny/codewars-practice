2b7a5c081f20bed5f40ac267347e154fd14c10b6
Apodlubny <apodlubny@gmail.com>
2022-12-23 22:30:34 +0200

    Update kata7.js

--- kata7.js
+++ kata7.js
@@ -125,6 +125,9 @@ console.log(validatePIN("123")); //false
 console.log(validatePIN("a234")); //false
 console.log(validatePIN("1111")); //true
 console.log(validatePIN("111123")); //true
+
+// ===================================================
+
 // 19. Jaden Casing Strings
 // Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
 
@@ -134,3 +137,4 @@ console.log(validatePIN("111123")); //true
 
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
