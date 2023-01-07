22652a521caf0d64276e3f8f060913a8430bf779
Apodlubny <apodlubny@gmail.com>
2023-01-07 09:20:17 +0200

    Simple multiplication

--- JS/kata8.js
+++ JS/kata8.js
@@ -274,3 +274,18 @@ console.log(feast("cat", "yogurt")); //false
 console.log(feast("blue-footed booby", "blueberry")); //true
 
 // ===================================================
+/* 
+22. Simple multiplication
+DESCRIPTION:
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+*/
+function simpleMultiplication(number){
+  return number%2 === 0 ? number * 8 : number * 9;
+}
+
+console.log(simpleMultiplication(2)); //16
+console.log(simpleMultiplication(1)); //9
+console.log(simpleMultiplication(8)); //64
+console.log(simpleMultiplication(5)); //45
