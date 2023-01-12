25ea713137f2364212818cad4ba72997b684ef19
Apodlubny <apodlubny@gmail.com>
2023-01-12 17:48:37 +0200

    minor

--- JS/kata8.js
+++ JS/kata8.js
@@ -336,3 +336,4 @@ console.log(lovefunc(1, 4)); // true
 console.log(lovefunc(2, 2)); // false
 console.log(lovefunc(0, 1)); // true
 console.log(lovefunc(0, 0)); // false
+// ===================================================
