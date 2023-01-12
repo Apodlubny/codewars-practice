a6a63a446880604f2d83a8bbd33eb0f08c6acb0c
Apodlubny <apodlubny@gmail.com>
2023-01-12 17:48:05 +0200

    25. Categorize New Member

--- JS/kata7.js
+++ JS/kata7.js
@@ -197,8 +197,58 @@ console.log(getDivisorsCnt(25)); //3
 // ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
 
 // MS
-const number = array => array.map((n, i) => `${i + 1}: ${n}`);
+const number = (array) => array.map((n, i) => `${i + 1}: ${n}`);
 
-console.log(number(["a", "b", "c"])) //["1: a", "2: b", "3: c"]);
+console.log(number(["a", "b", "c"])); //["1: a", "2: b", "3: c"]);
 
 // ===================================================
+/* 25. Categorize New Member
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+ */
+function openOrSenior(data) {
+  var result = [];
+  data.forEach(function (member) {
+    if (member[0] >= 55 && member[1] > 7) {
+      result.push("Senior");
+    } else {
+      result.push("Open");
+    }
+  });
+  return result;
+}
+//BP
+// function openOrSenior(data) {
+//   return data.map(([age, handicap]) =>
+//     age >= 55 && handicap > 7 ? "Senior" : "Open"
+//   );
+// }
+
+console.log(
+  openOrSenior([
+    [45, 12],
+    [55, 21],
+    [19, -2],
+    [104, 20],
+  ])
+); //['Open', 'Senior', 'Open', 'Senior']
+
+console.log(
+  openOrSenior([
+    [3, 12],
+    [55, 1],
+    [91, -2],
+    [53, 23],
+  ])
+); //['Open', 'Open', 'Open', 'Open']
