c98b8b0f4f15ac4a3db6873856d212529d6f799a
Apodlubny <apodlubny@gmail.com>
2023-01-15 18:36:54 +0200

    27. altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- JS/kata8.js
+++ JS/kata8.js
@@ -337,3 +337,25 @@ console.log(lovefunc(2, 2)); // false
 console.log(lovefunc(0, 1)); // true
 console.log(lovefunc(0, 0)); // false
 // ===================================================
+/* 
+27. altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+ */
+
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join("");
+};
+console.log(toAlternatingCase("hello world")); //"HELLO WORLD"
+console.log(toAlternatingCase("1a2b3c4d5e")); //"1A2B3C4D5E"
+console.log(toAlternatingCase("HeLLo WoRLD")); //"hEllO wOrld"
