6a2e165fe75060e0cc4b17251e02534f0797e730
Apodlubny <apodlubny@gmail.com>
2023-01-25 22:51:41 +0200

    11. Disemvowel Trolls

--- JS/kata7.js
+++ JS/kata7.js
@@ -301,3 +301,26 @@ console.log(descendingOrder(111)); //111
 console.log(descendingOrder(15)); //51
 console.log(descendingOrder(1021)); //2110
 console.log(descendingOrder(123456789)); //987654321
+// ===================================================
+/* 
+11. Disemvowel Trolls
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+
+function disemvowel(str) {
+  let regex = /[aiueoAIUEO]/g;
+  return str.replace(regex, "");
+}
+
+console.log(disemvowel("This website is for losers LOL!")); // "Ths wbst s fr lsrs LL!"
+console.log(
+  disemvowel("No offense but,\nYour writing is among the worst I've ever read")
+); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
