16fe1f80d396536b092f6215acd02b865c1e1549
Apodlubny <apodlubny@gmail.com>
2023-01-26 15:57:28 +0200

    12. Binary Addition

--- JS/kata7.js
+++ JS/kata7.js
@@ -325,5 +325,19 @@ console.log(
   disemvowel("No offense but,\nYour writing is among the worst I've ever read")
 ); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
 
-
 // ===================================================
+/* 12. Binary Addition
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+ */
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+console.log(addBinary(51, 12)); // "111111"
+console.log(addBinary(100, 0)); // "1100100"
