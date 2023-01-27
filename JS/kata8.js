10e544ebf302f95eeb05eb962558b532748b15d6
Apodlubny <apodlubny@gmail.com>
2023-01-27 20:55:12 +0200

    21. Grasshopper - Messi goals function

--- JS/kata8.js
+++ JS/kata8.js
@@ -410,3 +410,25 @@ String.prototype.isUpperCase = function() {
 console.log(isUpperCase("HELLO I AM DONALD")); //'HELLO I AM DONALD is upper case'
 console.log(isUpperCase("hello I AM DONALD")); //'HELLO I AM DONALD is  not upper case'
 // ==================================================================================
+/* 
+21. Grasshopper - Messi goals function
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+ */
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+console.log(goals(43, 10, 5)); //58
+console.log(goals(0, 0, 0)); // 0
