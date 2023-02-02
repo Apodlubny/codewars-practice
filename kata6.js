c25a44077a4f0697410924dd281ed9d5bd4c1198
Apodlubny <apodlubny@gmail.com>
2023-02-02 22:12:14 +0200

    2. Two Sum

--- kata6.js
+++ kata6.js
@@ -22,3 +22,28 @@ function order(words) {
 
 console.log(order("is2 Thi1s T4est 3a")); // "Thi1s is2 3a T4est"
 console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // "Fo1r the2 g3ood 4of th5e pe6ople"
+// ========================================================================================
+/* 
+2. Two Sum
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+
+twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0] */
+
+function twoSum(numbers, target) {
+  for (let i = 0; i <= numbers.length; i += 1) {
+    for (let j = i + 1; j <= numbers.length; j += 1) {
+      let result = numbers[i] + numbers[j];
+      if (result === target) return [i, j];
+    }
+  }
+}
+console.log(twoSum([1, 2, 3], 4)); // [0,2]
+console.log(twoSum([1234, 5678, 9012], 14690)); // [1,2]
+console.log(twoSum([2, 2, 3], 4)); // [0,1]
+// ========================================================================================
