6a4aebae034e1e4467570e92f32889eef040e38a
Apodlubny <apodlubny@gmail.com>
2023-02-05 18:56:09 +0200

    15. Exes and Ohs

--- JS/kata7.js
+++ JS/kata7.js
@@ -399,3 +399,24 @@ const sequenceSum = (begin, end, step) => {
 console.log(sequenceSum(2, 6, 2)); //12
 console.log(sequenceSum(1, 5, 1)); //12
 console.log(sequenceSum(0, 15, 3)); //12
+// ===================================================
+/* 
+15. Exes and Ohs
+ Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+ */
+function XO(str) {
+  return (
+    str.toLowerCase().split("x").length === str.toLowerCase().split("o").length
+  );
+}
+console.log(XO("xo")); //true
+console.log(XO("xxxxxxxooo")); //false
+console.log(XO("abcdefghijklmnopqrstuvwxyz")); //true,'Alphabet contains equal amount of x and o'
