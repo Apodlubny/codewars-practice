c741bb7884845790a46e75e11d86f41da6060920
Apodlubny <apodlubny@gmail.com>
2023-02-06 21:35:28 +0200

    5. Array.diff

--- kata6.js
+++ kata6.js
@@ -88,3 +88,24 @@ console.log(deleteNth([20, 37, 20, 21], 1)); // [20, 37, 21];
 console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)); // [1, 1, 3, 3, 7, 2, 2, 2]
 console.log(deleteNth([1, 1, 1, 1, 1], 5)); // [1,1,1,1,1];
 console.log(deleteNth([1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1], 3)); // [1, 2, 3, 1, 1, 2, 2, 3, 3, 4, 5]
+/* 
+5. Array.diff
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3] */
+
+function arrayDiff(a, b) {
+  return a.filter((e) => !b.includes(e));
+}
+
+console.log(arrayDiff([1, 2], [1])); //[2], "a was [1,2], b was [1]"
+console.log(arrayDiff([1, 2, 2], [1])); // [2,2], "a was [1,2,2], b was [1]");
+assert.deepEqual(
+  arrayDiff([1, 2, 2], [2]), // [1],"a was [1,2,2], b was [2]");
+  console.log(arrayDiff([], [1, 2])) // [], "a was [], b was [1,2]")
+);
