4379321bf5bae923a279b25cb3b082315f81b744
Apodlubny <apodlubny@gmail.com>
2023-02-21 19:09:12 +0200

    8. Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

--- JS/kata6.js
+++ JS/kata6.js
@@ -233,3 +233,92 @@ console.log(bouncingBall([2.0, 0.5, 1])); //1
 console.log(bouncingBall([3.0, 0.66, 1.5])); //3
 console.log(bouncingBall([30, 0.75, 1.5])); //21
 // ========================================================================================
+/* 8. Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+The number 
+89
+89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number: 
+89
+=
+8
+1
++
+9
+2
+89=8 
+1
+ +9 
+2
+ 
+
+The next number in having this property is 
+135
+135:
+
+See this property again: 
+135
+=
+1
+1
++
+3
+2
++
+5
+3
+135=1 
+1
+ +3 
+2
+ +5 
+3
+ 
+
+Task
+We need a function to collect these numbers, that may receive two integers 
+�
+a, 
+�
+b that defines the range 
+[
+�
+,
+�
+]
+[a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+Examples
+Let's see some cases (input -> output):
+
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+If there are no numbers of this kind in the range 
+[
+�
+,
+�
+]
+[a,b] the function should output an empty list.
+
+90, 100 --> []
+
+ */
+function sumDigPow(a, b) {
+  let result = [];
+  for (i = a; i <= b; i += 1) {
+    let number = String(i).split("");
+    if (
+      i ===
+      number.reduce(
+        (acc, currentValue, currentIndex) =>
+          acc + currentValue ** (currentIndex + 1),
+        0
+      )
+    )
+      result.push(i);
+  }
+  return result;
+}
+
+console.log(sumDigPow(1, 10)); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sumDigPow(10, 100)); //[89]
+console.log(sumDigPow(50, 150)); //[89, 135]
