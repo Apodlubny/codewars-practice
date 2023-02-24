c8cb80cfcdf11ded1b2e4e34ce27e148325f50c6
Apodlubny <apodlubny@gmail.com>
2023-02-24 19:41:09 +0200

    16. Is this a triangle?

--- JS/kata7.js
+++ JS/kata7.js
@@ -420,3 +420,15 @@ function XO(str) {
 console.log(XO("xo")); //true
 console.log(XO("xxxxxxxooo")); //false
 console.log(XO("abcdefghijklmnopqrstuvwxyz")); //true,'Alphabet contains equal amount of x and o'
+// ===================================================
+/* 16. Is this a triangle?
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted). */
+
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
+}
+console.log(isTriangle(1, 2, 3)); //false
+console.log(isTriangle(4, 2, 3)); //true
+console.log(isTriangle(-5, 1, 3)); //false
