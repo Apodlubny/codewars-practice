469d91c8a33115a24720ea1ba67fd2c1ccd687a7
Apodlubny <apodlubny@gmail.com>
2023-02-25 19:15:57 +0200

    26. Basic Mathematical Operations

--- JS/kata8.js
+++ JS/kata8.js
@@ -494,3 +494,47 @@ console.log(greet("Ryan")); //"Hello, Ryan how are you doing today?"
 function makeUpperCase(str) {
   return str.toUpperCase();
 }
+
+/* 
+26. Basic Mathematical Operations
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7 */
+
+function basicOp(operation, value1, value2) {
+  let result = "";
+  switch (operation) {
+    case "+":
+      result = value1 + value2;
+      break;
+    case "-":
+      result = value1 - value2;
+      break;
+    case "*":
+      result = value1 * value2;
+      break;
+    case "/":
+      result = value1 / value2;
+      break;
+    default:
+      return null;
+  }
+  return result;
+}
+
+// best practice
+/* 
+function basicOp(o, a, b) {
+  return eval(a + o + b);
+} 
+*/
+console.log(basicOp("+", 4, 7)); //11
+console.log(basicOp("-", 15, 18)); //-3
+console.log(basicOp("*", 5, 5)); //25
