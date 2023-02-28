8ec117707a956b97a7a4bae99f868772c8ecc522
Apodlubny <apodlubny@gmail.com>
2023-02-28 19:07:47 +0200

    Update kata7.js

--- JS/kata7.js
+++ JS/kata7.js
@@ -458,3 +458,15 @@ console.log(isIsogram("Dermatoglyphics")); //true
 console.log(isIsogram("isIsogram")); //false
 console.log(isIsogram("")); //true "an empty string is a valid isogram"
 // ===================================================
+/* 18. Get the Middle Character
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A" */
