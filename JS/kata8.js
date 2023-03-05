cdefd95463b4dea8387cc836389682689cd88407
Apodlubny <apodlubny@gmail.com>
2023-03-05 20:14:18 +0200

    31. Cat years, Dog years

--- JS/kata8.js
+++ JS/kata8.js
@@ -593,3 +593,59 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return i * 2;
 }
+// ==================================================================================
+/* 31. Cat years, Dog years
+I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+ */
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+
+  if (humanYears >= 1) {
+    catYears += 15;
+    dogYears += 15;
+  }
+  if (humanYears >= 2) {
+    catYears += 9;
+    dogYears += 9;
+  }
+  if (humanYears > 2) {
+    catYears += (humanYears - 2) * 4;
+    dogYears += (humanYears - 2) * 5;
+  }
+
+  return [humanYears, catYears, dogYears];
+};
+//best practice
+/*
+ function calculateAgeInYears(humanYears) {
+  if (humanYears < 1) {
+    return [0, 0, 0];
+  }
+
+  let catYears = 15 + (humanYears > 1 ? 9 + (humanYears - 2) * 4 : 0);
+  let dogYears = 15 + (humanYears > 1 ? 9 + (humanYears - 2) * 5 : 0);
+
+  return [humanYears, catYears, dogYears];
+} */
+console.log(humanYearsCatYearsDogYears(1)); // [1,15,15]
+console.log(humanYearsCatYearsDogYears(2)); // [2,24,24]
+console.log(humanYearsCatYearsDogYears(10)); // [10,56,64]
