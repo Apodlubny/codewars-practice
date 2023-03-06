0b13149b752acab4550ab7eff64d6a3d2c007876
Apodlubny <apodlubny@gmail.com>
2023-03-06 19:12:30 +0200

    20. Vowel Count

--- JS/kata7.js
+++ JS/kata7.js
@@ -500,3 +500,18 @@ console.log(reverseWords("apple")); //'elppa'
 console.log(reverseWords("double  spaced  words")); //'elbuod  decaps  sdrow'
 
 //===================================================
+/* 
+20. Vowel Count
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+ */
+function getCount(str) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  return str.split("").filter((letter) => vowels.includes(letter)).length;
+}
+console.log(getCount("abracadabra")); //5
+console.log(getCount("pear tree")); //4
+//===================================================
