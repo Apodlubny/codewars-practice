22a13d29d6d9a134e7bb4433b66c5704ef26fbb4
Apodlubny <apodlubny@gmail.com>
2023-03-29 22:19:24 +0300

    35. Even or Odd

--- JS/kata8.js
+++ JS/kata8.js
@@ -713,3 +713,12 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
 function boolToWord(bool) {
   return bool ? "Yes" : "No";
 }
+
+/* 35. Even or Odd
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+console.log(evenOrOdd(2)); //"Even"
+console.log(evenOrOdd(3)); //"Odd"
