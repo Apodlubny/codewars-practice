cd523bfc9aff83509b90a3526fa8ba7d2da29efb
Apodlubny <apodlubny@gmail.com>
2023-04-02 21:39:32 +0300

    12. Counting Duplicates

--- JS/kata6.js
+++ JS/kata6.js
@@ -430,3 +430,41 @@ function duplicateEncode(word){
 
 console.log(duplicateEncode("recede")); //"()()()",
 console.log(duplicateEncode("Success")); //")())())",
+
+/* 
+12. Counting Duplicates 
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic
+ characters and numeric digits that occur more than once in the input string. 
+ The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice*/
+
+function duplicateCount(str) {
+  const charCount = {};
+  let count = 0;
+
+  for (const char of str.toLowerCase()) {
+    if (charCount[char]) {
+      charCount[char]++;
+      if (charCount[char] === 2) {
+        count++;
+      }
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  return count;
+}
+
+console.log(duplicateCount("abcde")); //0
+console.log(duplicateCount("aabbcde")); //2
+console.log(duplicateCount("Indivisibility")); //1
