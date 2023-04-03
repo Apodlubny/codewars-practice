937f65240c2dd978f818e4b56db916aeab59b5e6
Apodlubny <apodlubny@gmail.com>
2023-04-03 20:56:43 +0300

    37. Function 1 - hello world

--- JS/kata8.js
+++ JS/kata8.js
@@ -737,3 +737,13 @@ n=-5, m=5:  0
 function paperwork(n, m) {
   return n > 0 && m > 0 ? n * m : 0;
 }
+
+/* 37. Function 1 - hello world
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of?
+ What is a "hello world" solution you would want to show your friends?
+ */
+
+const greet = () => "hello world!";
