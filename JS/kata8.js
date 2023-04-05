77c2a47bcdd591f5602ebbd2ce0788df0b5d9f39
Apodlubny <apodlubny@gmail.com>
2023-04-05 22:32:21 +0300

    39. Square(n) Sum

--- JS/kata8.js
+++ JS/kata8.js
@@ -792,3 +792,12 @@ function switchItUp(number) {
 
   return numberWords[number] || "";
 }
+
+/* 39. Square(n) Sum
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9  */
+
+function squareSum(numbers) {
+  return numbers.reduce((sum, number) => sum + number ** 2, 0);
+}
