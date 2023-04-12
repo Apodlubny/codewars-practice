c8ea315c633ee3d5b7c0b2dec1db26ca9ae78196
Apodlubny <apodlubny@gmail.com>
2023-04-12 21:39:16 +0300

    47. Third Angle of a Triangle

--- JS/kata8-2.js
+++ JS/kata8-2.js
@@ -297,3 +297,16 @@ move(3, 6) should equal 15 */
 function move(position, roll) {
   return position + roll * 2;
 }
+
+/* 47. Third Angle of a Triangle
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+ */
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
