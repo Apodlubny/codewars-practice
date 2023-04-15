e0846b1b9b3bf71f1b787a37fe1680da702864c8
Apodlubny <apodlubny@gmail.com>
2023-04-15 21:24:54 +0300

    51. Sort and Star

--- JS/kata8-2.js
+++ JS/kata8-2.js
@@ -372,3 +372,42 @@ const rps = (p1, p2) => {
     return "Player 2 won!";
   }
 };
+
+/* 51. Sort and Star
+You will be given a list of strings. You must sort it alphabetically (case-sensitive,
+ and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array. */
+function twoSort(s) {
+  return s.sort()[0].split("").join("***");
+}
+
+console.log(
+  twoSort([
+    "bitcoin",
+    "take",
+    "over",
+    "the",
+    "world",
+    "maybe",
+    "who",
+    "knows",
+    "perhaps",
+  ])
+); //'b***i***t***c***o***i***n'
+console.log(
+  twoSort([
+    "i",
+    "want",
+    "to",
+    "travel",
+    "the",
+    "world",
+    "writing",
+    "code",
+    "one",
+    "day",
+  ])
+); //'c***o***d***e'
