d1a7baa80da5c17f8732881dffc1679909269ea7
Apodlubny <apodlubny@gmail.com>
2023-04-16 21:27:25 +0300

    13. Count the smiley faces!

--- JS/kata6.js
+++ JS/kata6.js
@@ -468,3 +468,31 @@ function duplicateCount(str) {
 console.log(duplicateCount("abcde")); //0
 console.log(duplicateCount("aabbcde")); //2
 console.log(duplicateCount("Indivisibility")); //1
+
+/* 13. Count the smiley faces!
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+Note
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array).
+ Order of the face (eyes, nose, mouth) elements will always be the same.
+
+
+ */
+
+function countSmileys(arr) {
+  return arr.filter((face) => face.match(/^[:;][-~]?[)D]$/)).length;
+}
