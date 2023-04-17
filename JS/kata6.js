59c3494c5c4073166187229fbbe382aca6d05a30
Apodlubny <apodlubny@gmail.com>
2023-04-17 21:02:04 +0300

    14. Highest Scoring Word

--- JS/kata6.js
+++ JS/kata6.js
@@ -496,3 +496,43 @@ In case of an empty array return 0. You will not be tested with invalid input (i
 function countSmileys(arr) {
   return arr.filter((face) => face.match(/^[:;][-~]?[)D]$/)).length;
 }
+
+/* 14. Highest Scoring Word
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+
+
+ */
+
+function high(str) {
+  const words = str.split(" ");
+  let highestScore = 0;
+  let highestWord = "";
+
+  words.forEach((word) => {
+    let score = 0;
+    for (let i = 0; i < word.length; i++) {
+      score += word.charCodeAt(i) - 96;
+    }
+    if (score > highestScore) {
+      highestScore = score;
+      highestWord = word;
+    }
+  });
+
+  return highestWord;
+}
+/* best practice
+function high(s){
+  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+} */
