0e4e92c058db229b665208cb898b433c70c01c2b
Apodlubny <apodlubny@gmail.com>
2023-04-19 23:56:26 +0300

    Update kata6.js

--- JS/kata6.js
+++ JS/kata6.js
@@ -536,3 +536,14 @@ function high(s){
   let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
   return s.split(' ')[as.indexOf(Math.max(...as))];
 } */
+
+/* 15. Unique In Order
+Implement the function unique_in_order which takes as argument a sequence and 
+returns a list of items without any elements with the same value next to each
+ other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3] */
