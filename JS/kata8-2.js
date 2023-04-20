9fdc48bade085bd29f3e57094b3a0a2b96688d36
Apodlubny <apodlubny@gmail.com>
2023-04-20 23:12:07 +0300

    57. Training JS #7: if..else and ternary operator

--- JS/kata8-2.js
+++ JS/kata8-2.js
@@ -495,3 +495,34 @@ var copaDelReyGoals = 5
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
 //=================================================================
+
+/* 57. Training JS #7: if..else and ternary operator
+Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1
+ parameter:n, n is the number of hotdogs a customer will buy, different numbers
+  have different prices (refer to the following table), return how much money will
+   the customer spend to buy that number of hotdogs.
+
+number of hotdogs	price per unit (cents)
+n < 5	100
+n >= 5 and n < 10	95
+n >= 10	90
+You can use if..else or ternary operator to complete it.
+
+ */
+
+function saleHotdogs(n){
+  if(n<5){
+  	return 100*n;
+  }else if((n >= 5)&&(n < 10)){
+  	return 95*n;
+  }else{
+  	return 90*n;
+  }
+}
+
+/* best practice
+function saleHotdogs(n){
+  return n*(n<5?100:n<10?95:90);
+}
+*/
+//=================================================================
