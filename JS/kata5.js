4d807650fe74489d5db230dee4be5565ba581d98
Apodlubny <apodlubny@gmail.com>
2023-04-21 14:29:54 +0300

    2. Calculating with Functions

--- JS/kata5.js
+++ JS/kata5.js
@@ -45,3 +45,107 @@ let incrementString = str => str.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 :
 
 console.log(incrementString("foobar000")); //"foobar001"
 console.log(incrementString("foobar00999")); //"foobar001000"
+//=======================================================================================
+
+/* 2. Calculating with Functions
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+seven(times(five())); // must return 35
+four(plus(nine())); // must return 13
+eight(minus(three())); // must return 5
+six(dividedBy(two())); // must return 3
+Requirements:
+
+There must be a function for each number from 0 ("zero") to 9 ("nine")
+There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+Each calculation consist of exactly one operation and two numbers
+The most outer function represents the left operand, the most inner function represents the right operand
+Division should be integer division. For example, this should return 2, not 2.666666...:
+ */
+
+function one(operation) {
+  if (!operation) return 1;
+  return operation(1);
+}
+
+function two(operation) {
+  if (!operation) return 2;
+  return operation(2);
+}
+
+function three(operation) {
+  if (!operation) return 3;
+  return operation(3);
+}
+
+function four(operation) {
+  if (!operation) return 4;
+  return operation(4);
+}
+
+function five(operation) {
+  if (!operation) return 5;
+  return operation(5);
+}
+
+function six(operation) {
+  if (!operation) return 6;
+  return operation(6);
+}
+
+function seven(operation) {
+  if (!operation) return 7;
+  return operation(7);
+}
+
+function eight(operation) {
+  if (!operation) return 8;
+  return operation(8);
+}
+
+function nine(operation) {
+  if (!operation) return 9;
+  return operation(9);
+}
+
+function plus(num) {
+  return function (operand) {
+    return operand + num;
+  };
+}
+
+function minus(num) {
+  return function (operand) {
+    return operand - num;
+  };
+}
+
+function times(num) {
+  return function (operand) {
+    return operand * num;
+  };
+}
+
+function dividedBy(num) {
+  return function (operand) {
+    return Math.floor(operand / num);
+  };
+}
+/* best practice
+function zero(fn) {return fn ? fn(0) : 0}
+function one(fn) {return fn ? fn(1) : 1}
+function two(fn) {return fn ? fn(2) : 2}
+function three(fn) {return fn ? fn(3) : 3}
+function four(fn) {return fn ? fn(4) : 4}
+function five(fn) {return fn ? fn(5) : 5}
+function six(fn) {return fn ? fn(6) : 6}
+function seven(fn) {return fn ? fn(7) : 7}
+function eight(fn) {return fn ? fn(8) : 8}
+function nine(fn) {return fn ? fn(9) : 9}
+
+function plus(n) {return function(v) {return v + n}}
+function minus(n) {return function(v) {return v - n}}
+function times(n) {return function(v) {return v * n}}
+function dividedBy(n) {return function(v) {return v / n}} */
+
+//=======================================================================================
