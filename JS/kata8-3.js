b7a60ac848cf872c7bbd097d4ddfea660e78f228
Apodlubny <apodlubny@gmail.com>
2023-04-23 13:30:02 +0300

    62. Welcome!

--- JS/kata8-3.js
+++ JS/kata8-3.js
@@ -1,4 +1,4 @@
-/* 1. Correct the mistakes of the character recognition software
+/* 61. Correct the mistakes of the character recognition software
 Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
 
 When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
@@ -16,3 +16,41 @@ function correct(string) {
     return string.replace(/[501]/g, match => corrections[match]);
   }
   //=====================================================================
+/* 62. Welcome!
+Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task
+Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+ */
+
+// Language database as a Map
+const languageDatabase = new Map([
+    ['english', 'Welcome'],
+    ['czech', 'Vitejte'],
+    ['danish', 'Velkomst'],
+    ['dutch', 'Welkom'],
+    ['estonian', 'Tere tulemast'],
+    ['finnish', 'Tervetuloa'],
+    ['flemish', 'Welgekomen'],
+    ['french', 'Bienvenue'],
+    ['german', 'Willkommen'],
+    ['irish', 'Failte'],
+    ['italian', 'Benvenuto'],
+    ['latvian', 'Gaidits'],
+    ['lithuanian', 'Laukiamas'],
+    ['polish', 'Witamy'],
+    ['spanish', 'Bienvenido'],
+    ['swedish', 'Valkommen'],
+    ['welsh', 'Croeso']
+  ]);
+  
+  
+  function greet(language) {
+      // Convert the input language parameter to lowercase
+    language = language.toLowerCase();
+      // Check if the language exists in the language database
+  return languageDatabase.has(language)? languageDatabase.get(language): languageDatabase.get('english')
+    
+  }
+    //=====================================================================
