fea95635806659ababfbe329c3a1b32783167a96
Apodlubny <apodlubny@gmail.com>
2023-04-24 22:49:29 +0300

    63. Convert a Number to a String!

--- JS/kata8-3.js
+++ JS/kata8-3.js
@@ -12,10 +12,10 @@ The test cases contain numbers only by mistake.
  */
 
 function correct(string) {
-    const corrections = { 5: 'S', 0: 'O', 1: 'I' };
-    return string.replace(/[501]/g, match => corrections[match]);
-  }
-  //=====================================================================
+  const corrections = { 5: "S", 0: "O", 1: "I" };
+  return string.replace(/[501]/g, (match) => corrections[match]);
+}
+//=====================================================================
 /* 62. Welcome!
 Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
 
@@ -26,31 +26,57 @@ Write a 'welcome' function that takes a parameter 'language' (always a string), 
 
 // Language database as a Map
 const languageDatabase = new Map([
-    ['english', 'Welcome'],
-    ['czech', 'Vitejte'],
-    ['danish', 'Velkomst'],
-    ['dutch', 'Welkom'],
-    ['estonian', 'Tere tulemast'],
-    ['finnish', 'Tervetuloa'],
-    ['flemish', 'Welgekomen'],
-    ['french', 'Bienvenue'],
-    ['german', 'Willkommen'],
-    ['irish', 'Failte'],
-    ['italian', 'Benvenuto'],
-    ['latvian', 'Gaidits'],
-    ['lithuanian', 'Laukiamas'],
-    ['polish', 'Witamy'],
-    ['spanish', 'Bienvenido'],
-    ['swedish', 'Valkommen'],
-    ['welsh', 'Croeso']
-  ]);
-  
-  
-  function greet(language) {
-      // Convert the input language parameter to lowercase
-    language = language.toLowerCase();
-      // Check if the language exists in the language database
-  return languageDatabase.has(language)? languageDatabase.get(language): languageDatabase.get('english')
-    
-  }
-    //=====================================================================
+  ["english", "Welcome"],
+  ["czech", "Vitejte"],
+  ["danish", "Velkomst"],
+  ["dutch", "Welkom"],
+  ["estonian", "Tere tulemast"],
+  ["finnish", "Tervetuloa"],
+  ["flemish", "Welgekomen"],
+  ["french", "Bienvenue"],
+  ["german", "Willkommen"],
+  ["irish", "Failte"],
+  ["italian", "Benvenuto"],
+  ["latvian", "Gaidits"],
+  ["lithuanian", "Laukiamas"],
+  ["polish", "Witamy"],
+  ["spanish", "Bienvenido"],
+  ["swedish", "Valkommen"],
+  ["welsh", "Croeso"],
+]);
+
+function greet(language) {
+  // Convert the input language parameter to lowercase
+  language = language.toLowerCase();
+  // Check if the language exists in the language database
+  return languageDatabase.has(language)
+    ? languageDatabase.get(language)
+    : languageDatabase.get("english");
+}
+//=====================================================================
+/* 63. Convert a Number to a String!
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+ */
+
+function numberToString(num) {
+  return num.toString();
+}
+// function numberToString(num) {
+//   return ""+num
+// }
+// function numberToString(num) {
+// //   return `${num}`
+// // }
+// function numberToString(num) {
+// //   return `${num}`
+// // }
+// function numberToString(num) {
+// //   return String.num
+// // }
