a355f297a52e63f58d71590ca05dd38810c6d917
Apodlubny <apodlubny@gmail.com>
2023-04-25 22:46:59 +0300

    64. Is it even?

--- JS/kata8-3.js
+++ JS/kata8-3.js
@@ -80,3 +80,19 @@ function numberToString(num) {
 // function numberToString(num) {
 // //   return String.num
 // // }
+
+//=========================================================================
+/* 64. Is it even?
+n this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata. */
+
+function testEven(n) {
+  return n % 2 === 0 ? true : false;
+}
