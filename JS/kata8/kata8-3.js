c171ef1a2a0ac1c94d234daef25a320e7b2dd4c6
Apodlubny <apodlubny@gmail.com>
2023-04-26 22:15:39 +0300

    Update kata8-3.js

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -96,3 +96,20 @@ Floats with decimal part non equal to zero are considered UNeven for this kata. 
 function testEven(n) {
   return n % 2 === 0 ? true : false;
 }
+//=========================================================================
+/* 
+65. Beginner Series #4 Cockroach
+The cockroach is one of the fastest insects. Write a function which takes its
+ speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer. */
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.7778);
+}
+
+//best practice const cockroachSpeed = s => Math.floor(s / 0.036);
+//=========================================================================
