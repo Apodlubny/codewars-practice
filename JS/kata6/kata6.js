c5a32b60b2eb4b61af4c41ffcb71366ea91e3b08
Apodlubny <apodlubny@gmail.com>
2023-04-29 18:53:39 +0300

    17. Equal Sides Of An Array

--- JS/kata6/kata6.js
+++ JS/kata6/kata6.js
@@ -430,6 +430,7 @@ function duplicateEncode(word){
 
 console.log(duplicateEncode("recede")); //"()()()",
 console.log(duplicateEncode("Success")); //")())())",
+// =============================================================================
 
 /* 
 12. Counting Duplicates 
@@ -469,6 +470,8 @@ console.log(duplicateCount("abcde")); //0
 console.log(duplicateCount("aabbcde")); //2
 console.log(duplicateCount("Indivisibility")); //1
 
+// =============================================================================
+
 /* 13. Count the smiley faces!
 Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
 
@@ -496,6 +499,7 @@ In case of an empty array return 0. You will not be tested with invalid input (i
 function countSmileys(arr) {
   return arr.filter((face) => face.match(/^[:;][-~]?[)D]$/)).length;
 }
+// =============================================================================
 
 /* 14. Highest Scoring Word
 Given a string of words, you need to find the highest scoring word.
@@ -536,6 +540,7 @@ function high(s){
   let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
   return s.split(' ')[as.indexOf(Math.max(...as))];
 } */
+// =============================================================================
 
 /* 15. Unique In Order
 Implement the function unique_in_order which takes as argument a sequence and 
@@ -560,6 +565,7 @@ var uniqueInOrder = function (iterable) {
   }
   return uniqueSeq;
 };
+// =============================================================================
 
 /* 16. Mexican Wave
 The wave (known as the Mexican wave in the English-speaking world outside North America)
@@ -594,4 +600,54 @@ function wave(str) {
     }
   }
   return result;
+}
+// =============================================================================
+
+/* 17. Equal Sides Of An Array
+
+You are going to be given an array of integers. Your job is to take that array and find an index N
+ where the sum of the integers to the left of N is equal to the sum of the integers to the right 
+ of N. If there is no index that would make this happen, return -1.
+
+For example:
+
+Let's say you are given the array {1,2,3,4,3,2,1}:
+Your function will return the index 3, because at the 3rd position of the array, the sum of left 
+side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+Let's look at another one.
+You are given the array {1,100,50,-51,1,1}:
+Your function will return the index 1, because at the 1st position of the array, the sum of left side
+ of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+Last one:
+You are given the array {20,10,-80,10,10,15,35}
+At index 0 the left side is {}
+The right side is {10,-80,10,10,15,35}
+They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+Index 0 is the place where the left side and right side are equal.
+
+Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+Input:
+An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+Output:
+The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find
+ an index that fits these rules, then you will return -1.
+
+Note:
+If you are given an array with multiple answers, return the lowest correct index.*/
+
+function findEvenIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const leftSum = arr.slice(0, i).reduce((acc, curr) => acc + curr, 0);
+    const rightSum = arr.slice(i + 1).reduce((acc, curr) => acc + curr, 0);
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+
+  return -1;
 }
