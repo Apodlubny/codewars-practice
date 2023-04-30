ea0b881b7f9d0fe934649b129644220485935673
Apodlubny <apodlubny@gmail.com>
2023-04-30 21:20:26 +0300

    18. Build Tower

--- JS/kata6/kata6.js
+++ JS/kata6/kata6.js
@@ -651,3 +651,37 @@ function findEvenIndex(arr) {
 
   return -1;
 }
+// =============================================================================
+
+/* 18. Build Tower
+Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number
+ of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+] */
+
+function towerBuilder(nFloors) {
+  const tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    const spaces = " ".repeat(nFloors - i - 1);
+    const stars = "*".repeat(i * 2 + 1);
+    tower.push(spaces + stars + spaces);
+  }
+  return tower;
+}
