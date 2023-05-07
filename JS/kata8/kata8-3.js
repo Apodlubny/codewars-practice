a62ad064739c89e51838af289217c02cdd8a2770
Apodlubny <apodlubny@gmail.com>
2023-05-07 19:35:06 +0300

    70. Exclamation marks series #1

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -172,3 +172,20 @@ function mouthSize(animal) {
   return animal.toLowerCase() === "alligator" ? "small" : "wide";
 }
 //=========================================================================
+/* 70. Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+Examples
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi"*/
+
+function remove(string) {
+  return string.endsWith("!") ? string.slice(0, -1) : string;
+}
+
+//=========================================================================
