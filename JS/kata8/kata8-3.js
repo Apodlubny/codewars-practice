7d2ad4f972a44ec676980e0238026a8bd23b9e82
Apodlubny <apodlubny@gmail.com>
2023-05-10 22:20:03 +0300

    74. Count of positives / sum of negatives

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -240,3 +240,40 @@ class SmallestIntegerFinder {
   }
 }
 //=========================================================================
+/* 74. Count of positives / sum of negatives
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element
+ is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]. */
+
+function countPositivesSumNegatives(input) {
+  let positive = 0;
+  let negative = 0;
+  if (!input || input.length === 0) {
+    return [];
+  }
+  for (let i = 0; i < input.length; i += 1) {
+    if (input[i] > 0) {
+      positive += 1;
+    }
+    if (input[i] < 0) {
+      negative += input[i];
+    }
+  }
+  return [positive, negative];
+}
+
+/* best practice
+function countPositivesSumNegatives(input) {
+  if (!Array.isArray(input) || !input.length) return [];
+  return input.reduce((arr, n) => {
+    if (n > 0) arr[0]++;
+    if (n < 0) arr[1] += n;
+    return arr;
+  }, [0, 0]);
+} */
