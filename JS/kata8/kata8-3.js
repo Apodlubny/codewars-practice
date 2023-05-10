e3af1e46bea0e06f35e630d650027fa6e2703e64
Apodlubny <apodlubny@gmail.com>
2023-05-10 22:28:16 +0300

    75. Sum Mixed Array

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -277,3 +277,15 @@ function countPositivesSumNegatives(input) {
     return arr;
   }, [0, 0]);
 } */
+
+//=========================================================================
+/* 75. Sum Mixed Array
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. */
+
+function sumMix(x) {
+  return x.reduce((acc, cur) => acc + Number(cur), 0);
+}
+
+//=========================================================================
