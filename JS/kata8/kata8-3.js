329d4a6ddeab3cc4d136d42822d6ade332053a3e
Apodlubny <apodlubny@gmail.com>
2023-05-14 12:10:28 +0300

    81. Calculate BMI

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -366,3 +366,31 @@ const isPalindrome = (x) => {
     : false;
 };
 //=========================================================================
+/* 81. Calculate BMI
+Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese" */
+
+function bmi(weight, height) {
+  const bmiIndex = weight / height ** 2;
+
+  if (bmiIndex <= 18.5) {
+    return "Underweight";
+  }
+  if (bmiIndex <= 25) {
+    return "Normal";
+  }
+  if (bmiIndex <= 30) {
+    return "Overweight";
+  }
+  if (bmiIndex > 30) {
+    return "Obese";
+  }
+}
+//=========================================================================
