c02b2e93ce470b5b78186584ea5473e95bd62de1
Apodlubny <apodlubny@gmail.com>
2023-05-18 21:54:10 +0300

    85. Hex to Decimal

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -452,3 +452,9 @@ function booleanToString(b) {
   return b.toString();
 }
 //=========================================================================
+/* 85. Hex to Decimal
+Complete the function which converts hex number (given as a string) to a decimal number. */
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
+//=========================================================================
