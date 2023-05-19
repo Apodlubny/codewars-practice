b02c9727dfbabfe894f4bb784e8efa09f5d88909
Apodlubny <apodlubny@gmail.com>
2023-05-19 22:03:40 +0300

    88. Super Duper Easy

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -475,3 +475,9 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   return mpg * fuelLeft >= distanceToPump ? true : false;
 };
 //=========================================================================
+/* 88. Super Duper Easy
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error". */
+function problem(x) {
+  return typeof x === "string" ? "Error" : x * 50 + 6;
+}
+//=========================================================================
