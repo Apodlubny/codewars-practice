69f9064d6d8f20f66459d2f80a1e51b4ecc197bc
Apodlubny <apodlubny@gmail.com>
2023-05-20 15:58:00 +0300

    39. Maximum Length Difference

--- JS/kata7/kata7-2.js
+++ JS/kata7/kata7-2.js
@@ -220,3 +220,36 @@ function calculateYears(principal, interest, tax, desired) {
   return years;
 }
 //======================================================================//
+/* 39. Maximum Length Difference
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+Bash note:
+input : 2 strings with substrings separated by ,
+output: number as a string */
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) {
+    return -1;
+  }
+
+  const lengths1 = a1.map((x) => x.length);
+  const lengths2 = a2.map((y) => y.length);
+
+  const max1 = Math.max(...lengths1);
+  const min1 = Math.min(...lengths1);
+
+  const max2 = Math.max(...lengths2);
+  const min2 = Math.min(...lengths2);
+
+  return Math.max(max1 - min2, max2 - min1);
+}
+
+//======================================================================//
