11080e977c49b6b74d0160552ce6b6f2cee7b3fc
Apodlubny <apodlubny@gmail.com>
2023-05-21 14:09:19 +0300

    90. Is he gonna survive?

--- JS/kata8/kata8-3.js
+++ JS/kata8/kata8-3.js
@@ -488,3 +488,13 @@ function combat(health, damage) {
   return health - damage < 0 ? 0 : health - damage;
 }
 //=========================================================================
+/* 90. Is he gonna survive?
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful
+ dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :) */
+
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2 ? true : false;
+}
+//=========================================================================
