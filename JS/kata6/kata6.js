c51d749bcd7e9e0530d08783664c94000281b0ae
Apodlubny <apodlubny@gmail.com>
2023-05-26 22:36:46 +0300

    21. Count characters in your string

--- JS/kata6/kata6.js
+++ JS/kata6/kata6.js
@@ -738,3 +738,22 @@ function persistence(num) {
 
   return 1 + persistence(result);
 }
+// =====================================================================================================================
+
+/* 21. Count characters in your string
+ The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.*/
+function count(string) {
+  const result = {};
+
+  for (let char of string) {
+    if (result[char]) {
+      result[char]++;
+    } else {
+      result[char] = 1;
+    }
+  }
+  return result;
+}
+// =====================================================================================================================
