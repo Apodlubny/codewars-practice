d5ae65e39c81caee85172a86693e86dbc1c1e4a1
Apodlubny <apodlubny@gmail.com>
2023-05-27 21:28:53 +0300

    94. Enumerable Magic #25

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -58,3 +58,9 @@ function switchItUp(number) {
 /* best practice
 switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n] */
 //=========================================================================================================================
+/* 94. Enumerable Magic #25 - Take the First N Elements
+ Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.*/
+function take(arr, n) {
+  return arr.slice(0, n);
+}
+//=========================================================================================================================
