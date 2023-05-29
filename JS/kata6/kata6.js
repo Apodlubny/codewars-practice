3f1ced9eaa6c700aac391229f06d946fce655d1b
Apodlubny <apodlubny@gmail.com>
2023-05-29 22:04:11 +0300

    22. Sort the odd

--- JS/kata6/kata6.js
+++ JS/kata6/kata6.js
@@ -757,3 +757,16 @@ function count(string) {
   return result;
 }
 // =====================================================================================================================
+/* 22. Sort the odd
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0] */
+
+function sortArray(array) {
+  const odds = array.filter((num) => num % 2 !== 0).sort((a, b) => a - b);
+  return array.map((num) => (num % 2 !== 0 ? odds.shift() : num));
+}
+// =====================================================================================================================
