cf09933b6b1b00f6fb723d31040908e05479dcb0
Apodlubny <apodlubny@gmail.com>
2023-06-01 21:54:40 +0300

    98. Fix your code before the garden dies!

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -139,3 +139,14 @@ function warnTheSheep(queue) {
     : `Oi! Sheep number ${wIndex}! You are about to be eaten by a wolf!`;
 }
 //=========================================================================================================================
+/* 98. Fix your code before the garden dies!
+You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of 
+water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
+
+Your task is to debug the code before your plants die! */
+function rainAmount(mm) {
+  return mm >= 40
+    ? "Your plant has had more than enough water for today!"
+    : `You need to give your plant ${40 - mm}mm of water`;
+}
+//=========================================================================================================================
