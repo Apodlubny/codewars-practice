be79c21a60e04c3d5b2b8ecb47dc748cc09838e2
Apodlubny <apodlubny@gmail.com>
2023-06-02 21:11:47 +0300

    99. Grasshopper - Function syntax debugging

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -150,3 +150,10 @@ function rainAmount(mm) {
     : `You need to give your plant ${40 - mm}mm of water`;
 }
 //=========================================================================================================================
+/* 99. Grasshopper - Function syntax debugging
+Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them. */
+function main(verb, noun) {
+  return `${verb}${noun}`;
+}
+//=========================================================================================================================
