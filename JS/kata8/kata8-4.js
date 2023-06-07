a20b8a42adb22b44bbc8feaaece087a919985c96
Apodlubny <apodlubny@gmail.com>
2023-06-07 21:50:53 +0300

    103. Enumerable Magic - Does My List Include This?

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -216,3 +216,8 @@ don't worry about uppercase vowels
 y is not considered a vowel for this kata */
 const shortcut = (string) => string.replace(/[aoiue]/g, "");
 //=========================================================================================================================
+/* 103. Enumerable Magic - Does My List Include This?
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+ */
+const include = (arr, item) => arr.includes(item);
+//=========================================================================================================================
