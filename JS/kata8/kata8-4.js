d3425028ec36977d8ea0c17db1858abb9d022916
Apodlubny <apodlubny@gmail.com>
2023-06-14 12:44:28 +0300

    106. Beginner - Lost Without a Map

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -248,3 +248,14 @@ function replace(s) {
   return s.replace(/[aoiueAOIEU]/g, "!");
 }
 //=========================================================================================================================
+/* 106. Beginner - Lost Without a Map 
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] */
+
+function maps(x){
+  return x.map(n => n * 2);
+}
+//=========================================================================================================================
