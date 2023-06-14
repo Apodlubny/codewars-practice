3940feb38a466e21980ea689e8b57d4c60e62d6c
Apodlubny <apodlubny@gmail.com>
2023-06-14 12:48:50 +0300

    24. Multiplication table

--- JS/kata6/kata6.js
+++ JS/kata6/kata6.js
@@ -804,3 +804,27 @@ function titleCase(title, minorWords) {
   return result.join(" ");
 }
 // =====================================================================================================================
+/* 24. Multiplication table
+Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9 */
+multiplicationTable = function(size) {
+  const table = [];
+  
+  for (let i = 1; i <= size; i+=1) {
+    const row = [];
+    
+    for (let j = 1; j <= size; j+=1) {
+      row.push(i * j);
+    }
+    
+    table.push(row);
+  }
+  
+  return table;
+}
+// =====================================================================================================================
