73e2e64aeb0a16004357f5d3413d10aa83f27159
Apodlubny <apodlubny@gmail.com>
2023-06-15 07:50:22 +0300

    46. Complementary DNA

--- JS/kata7/kata7-2.js
+++ JS/kata7/kata7-2.js
@@ -374,3 +374,25 @@ The [0] index access is used to retrieve the first element of the resulting arra
 For example, if the url is "www.codewars.com#about", split('#')[0] will return "www.codewars.com".
 The retrieved portion of the URL before the anchor is then returned as the result of the function.*/
 //==========================================================================================================================================//
+/* 46. Complementary DNA
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell);
+ you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+ */
+function DNAStrand(dna){
+  return dna.replace(/./g, function(match) {
+   return {
+     A: 'T',
+     T: 'A',
+     C: 'G',
+     G: 'C',
+   }[match];
+ });
+}
+//==========================================================================================================================================//
