3c2c671ba1a5dfdc5517f2223e6f8cd41874df60
Apodlubny <apodlubny@gmail.com>
2023-06-21 07:50:49 +0300

    47. Alternate capitalization

--- JS/kata7/kata7-2.js
+++ JS/kata7/kata7-2.js
@@ -396,3 +396,18 @@ function DNAStrand(dna){
  });
 }
 //==========================================================================================================================================//
+/* 47. Alternate capitalization
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck! */
+function capitalize(s){
+  const evenChars = s.split('').map((char, index) => (index % 2 === 0 ? char.toUpperCase() : char));
+ const oddChars = s.split('').map((char, index) => (index % 2 !== 0 ? char.toUpperCase() : char));
+
+ return [evenChars.join(''), oddChars.join('')];
+};
+//==========================================================================================================================================//
