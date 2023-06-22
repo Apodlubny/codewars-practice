9d45d9fba950d170c368b7d8f4fb97adc5d37412
Apodlubny <apodlubny@gmail.com>
2023-06-22 07:56:06 +0300

    108. Hello, Name or World!

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -270,3 +270,27 @@ For example(Input --> Output):
 "Zach" --> 18 */
 const howManyLightsabersDoYouOwn = name => name === 'Zach' ? 18 : 0;
 //=========================================================================================================================
+/* 108. Hello, Name or World!
+ Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"*/
+
+  const hello = s =>
+  `Hello, ${s ? (s[0].toUpperCase() + s.slice(1).toLowerCase()) : 'World'}!`;
+
+  /* function hello(name) {
+    if (name && name.length > 0) {
+   
+    const formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    return "Hello, " + formattedName + "!";
+  } else {
+    return "Hello, World!";
+  }
+} */
