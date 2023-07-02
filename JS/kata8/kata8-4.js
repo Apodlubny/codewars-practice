5da6c184cd2396f5018df17461e040b57ec41c27
Apodlubny <apodlubny@gmail.com>
2023-07-02 22:04:57 +0300

    110. Grasshopper - Array Mean

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -305,3 +305,21 @@ Array can contain numbers or strings. x can be either. Return true if the array 
 
 const check = (a,x) => a.includes(x);
 //=========================================================================================================================
+
+/* 110. Grasshopper - Array Mean
+Find Mean
+Find the mean (average) of a list of numbers in an array.
+Information
+To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+
+1+3+5+7 = 16
+2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+16/4 = 4
+3. The mean (or average) of this list is 4
+ */
+const findAverage = nums => nums.length === 0 ? 0 : nums.reduce((a,n) => n+a, 0)/nums.length
+  //=========================================================================================================================
