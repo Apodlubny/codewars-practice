037af6006dba3992e2c3070e088a0f8888183162
Apodlubny <apodlubny@gmail.com>
2023-07-30 23:49:51 +0300

    50. Predict your age!

--- JS/kata7/kata7-2.js
+++ JS/kata7/kata7-2.js
@@ -476,3 +476,24 @@ function wordsToMarks(str)
   return sum;
 }
 //==========================================================================================================================================//
+/* 50. Predict your age!
+My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+In honor of my grandfather's memory we will write a function using his formula!
+
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86 */
+function predictAge(...ages){
+  const predictedAge = Math.sqrt(ages.reduce((sum, age) => sum + age ** 2, 0)) / 2;
+  return Math.floor(predictedAge);
+}
+
+/* best prctice solution 
+const predictAge = (...ages) => Math.hypot(...ages) / 2 | 0;
+ */
+//==========================================================================================================================================//
