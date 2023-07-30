8ccba83becd062a2d5779a32ce1f7bd634b74975
Apodlubny <apodlubny@gmail.com>
2023-07-30 13:37:33 +0300

    112. Return the day

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -341,3 +341,20 @@ function checkForFactor (base, factor) {
   return base % factor === 0;
 }
   //=========================================================================================================================
+/* 112. Return the day
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+ */
+function whatday(num) { 
+  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return num >= 1 && num <= 7 ? days[num-1] : "Wrong, please enter a number between 1 and 7";
+}
+//=========================================================================================================================
