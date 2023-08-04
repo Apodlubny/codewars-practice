c6793e2cba9a64c8ae300817c14b98460db35f22
Apodlubny <apodlubny@gmail.com>
2023-08-04 15:32:55 +0300

    117. Define a card suit

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -460,3 +460,25 @@ function gooseFilter (birds) {
   // return an array containing all of the strings in the input array except those that match strings in geese
 };
 //=========================================================================================================================
+
+/* 117. Define a card suit
+You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades' */
+
+function defineSuit(card) {
+  if (card.includes('♣')) return 'clubs';
+  if (card.includes('♦')) return 'diamonds';
+  if (card.includes('♥')) return 'hearts';
+  if (card.includes('♠')) return 'spades';
+}
+/* solution using Object
+function defineSuit(card) {
+    return {'♣': 'clubs', '♠': 'spades', '♦': 'diamonds', '♥': 'hearts'}[card.slice(-1)];
+} */
+//=========================================================================================================================
