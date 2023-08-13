0e138de7504a6bfed098edc1ad011eee8c94ac7e
Apodlubny <apodlubny@gmail.com>
2023-08-13 22:29:04 +0300

    119. Name Shuffler

--- JS/kata8/kata8-4.js
+++ JS/kata8/kata8-4.js
@@ -503,3 +503,9 @@ function _if(bool, func1, func2) {
   return bool ? func1() : func2();
 }
 //=========================================================================================================================
+/* 119. Name Shuffler
+Write a function that returns a string in which firstname is swapped with last name.
+Example(Input --> Output)
+"john McClane" --> "McClane john" */
+const nameShuffler = (str) => str.split(" ").reverse().join(" ");
+//=========================================================================================================================
