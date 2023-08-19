7e9793a8e1e9ea2dec91e5f41e46e8a836dedf46
Apodlubny <apodlubny@gmail.com>
2023-08-19 13:09:01 +0300

    Пошук найдовшого слова в рядку

--- JS/freeCodeCamp.js
+++ JS/freeCodeCamp.js
@@ -40,11 +40,11 @@ reverseString("hello");
 Функції будуть задані тільки ті цілі числа, які більші або дорівнюють нулю. */
 //loop solution
 // function factorialize(num) {
-//   let product = 1;
+//   let result = 1;
 //   for (let i = 2; i <= num; i++) {
-//     product *= i;
+//     result *= i;
 //   }
-//   return product;
+//   return result;
 // }
 
 // factorialize(5);
@@ -58,4 +58,35 @@ function factorialize(num) {
 }
 
 factorialize(5);
+//******************************************************************************************************************************
+/* 
+Пошук найдовшого слова в рядку
+Поверніть довжину найдовшого слова у даному реченні.
+
+Ваша відповідь повинна бути числом. */
+
+//1 solution
+// function findLongestWordLength(str) {
+//   // split the string into individual words
+//   const words = str.split(" ");
+
+//   // words only has 1 element left that is the longest element
+//   if (words.length == 1) {
+//     return words[0].length;
+//   }
+
+//   // if words has multiple elements, remove the first element
+//   // and recursively call the function
+//   return Math.max(
+//     words[0].length,
+//     findLongestWordLength(words.slice(1).join(" "))
+//   );
+// }
+
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//map solution
+function findLongestWordLength(str) {
+  return Math.max(...str.split(" ").map((word) => word.length));
+}
 //******************************************************************************************************************************
