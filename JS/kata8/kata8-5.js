38242927e5b8a561bc66d160b70e059ca50c3861
Apodlubny <apodlubny@gmail.com>
2023-09-04 12:13:13 +0300

    125. Sentence Smash

--- JS/kata8/kata8-5.js
+++ JS/kata8/kata8-5.js
@@ -66,3 +66,14 @@ function lowercaseCount(str){
     return (str.match(/[a-z]/g) || []).length
 } */
 //====================================================================================================================//
+/* 125. Sentence Smash
+Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+ */
+smash = function (words) {
+  return words.join(" ");
+};
+//====================================================================================================================//
