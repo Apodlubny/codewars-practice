1baba4def1c4041e74b798073f194bd8182fa547
Apodlubny <apodlubny@gmail.com>
2023-09-05 13:41:15 +0300

    54. Parts of a list

--- JS/kata7/kata7-2.js
+++ JS/kata7/kata7-2.js
@@ -543,3 +543,35 @@ function isPowerOfTwo(n) {
   return n > 0 && (n & (n - 1)) === 0;
 }
 //==========================================================================================================================================//
+/* 54. Parts of a list
+Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+Examples of returns in different languages:
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+or
+ a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+{{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or 
+a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+"(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)" */
+
+function partlist(arr) {
+  const result = [];
+
+  for (let i = 1; i < arr.length; i += 1) {
+    const left = arr.slice(0, i).join(" ");
+    const right = arr.slice(i).join(" ");
+    result.push([left, right]);
+  }
+
+  return result;
+}
